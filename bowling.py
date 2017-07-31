5c4c5c0015105bf0f3524a20ab736fdc0c3c0dac
Kata <katalin.molnar.kiss@gmail.com>
2017-07-31 07:09:54 +0200

    pep8 and some merging

--- bowling.py
+++ bowling.py
@@ -1,15 +1,13 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_half = True
+    in_first_try = True
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
@@ -19,25 +17,24 @@ def score(game):
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
+        if not in_first_try:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
+        if in_first_try == True:
+            in_first_try = False
         else:
-            in_first_half = True
+            in_first_try = True
         if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
+            in_first_try = True
             frame += 1
     return result
+
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+            return int(char)
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
