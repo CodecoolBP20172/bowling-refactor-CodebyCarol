dbae3d3f173747415b4923cf143fc81d332e015f
Kata <katalin.molnar.kiss@gmail.com>
2017-08-06 07:08:21 +0200

    get_value a bit cleaner

--- bowling.py
+++ bowling.py
@@ -1,3 +1,6 @@
+# X means strike
+# / means spare
+# - means miss
 def score(game):
     result = 0
     frame = 1
@@ -28,15 +31,10 @@ def score(game):
             frame += 1
     return result
 
-
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-            return int(char)
-    elif char == 'X' or char == 'x' or char == '/':
+    if char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
-    else:
-        raise ValueError()
+    elif int(char) > 0 and int(char) < 10:
+        return int(char)
